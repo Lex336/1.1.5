6cf7d68a379864ef0b789ec32344ae8ffbe3ea6b
Lex336 <lex0389@gmail.com>
2023-06-28 23:33:10 +0300

    решение
    Signed-off-by: Lex336 <lex0389@gmail.com>

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,99 +3,90 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class UserDaoJDBCImpl implements UserDao {
-
     public UserDaoJDBCImpl() {
 
     }
 
-    @Override
     public void createUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS users " +
-                    "(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), last_name VARCHAR(50), age INT)"
-            );
-            Util.closeConnection(connection);
+        try (Connection connection = Util.createConnection()) {
+            connection.createStatement().executeUpdate(
+                    "CREATE TABLE IF NOT EXISTS users (" +
+                            "id bigint AUTO_INCREMENT NOT NULL," +
+                            "name varchar(100) NOT NULL," +
+                            "last_name varchar(100) NOT NULL," +
+                            "age tinyint NOT NULL," +
+                            "CONSTRAINT table_name_pk PRIMARY KEY (id)" +
+                            ");");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
     public void dropUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            connection.createStatement().executeUpdate("DROP TABLE IF EXISTS users");
-            Util.closeConnection(connection);
+        try (Connection connection = Util.createConnection()) {
+            connection.createStatement().executeUpdate(
+                    "DROP TABLE IF EXISTS users;");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getConnection()) {
-
+        try (Connection connection = Util.createConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)");
+                    "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?);");
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
+            preparedStatement.setByte(3 , age);
             preparedStatement.executeUpdate();
-            Util.closeConnection(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection()) {
-
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "DELETE FROM users WHERE id = ?");
+        try (Connection connection = Util.createConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
-            Util.closeConnection(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-
-        try (Connection connection = Util.getConnection()) {
-
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM users");
+        List<User> userList = new ArrayList<>();
+        try (Connection connection = Util.createConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users");
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                User user = new User(
-                        resultSet.getString("name"),
-                        resultSet.getString("last_name"),
-                        resultSet.getByte("age"));
 
-                user.setId(resultSet.getLong("id"));
-                users.add(user);
+            while (resultSet.next()) {
+                long id = resultSet.getByte("id");
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("last_name");
+                byte age = resultSet.getByte("age");
+
+                userList.add(new User(name, lastName, age));
             }
-            Util.closeConnection(connection);
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-        return users;
+        return userList;
     }
 
-    @Override
     public void cleanUsersTable() {
-        try (Connection connection = Util.getConnection()) {
-            connection.createStatement().executeUpdate("TRUNCATE TABLE users");
-            Util.closeConnection(connection);
+        try (Connection connection = Util.createConnection()) {
+
+            connection.createStatement().executeUpdate("TRUNCATE users");
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,31 +1,55 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.hibernate.cfg.AvailableSettings.USER;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/kata";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "aA5771521";
+    private static SessionFactory sessionFactory;
 
-    public static Connection getConnection() {
+    public static SessionFactory getSessionFactory(){
+        if (sessionFactory == null){
+            try{
+                Properties setting = new Properties();
+                setting.put(Environment.URL, URL);
+                setting.put(Environment.USER, USERNAME);
+                setting.put(Environment.PASS, PASSWORD);
+                setting.put(Environment.DIALECT, "org.hibernate.dialect.MyAQLDialect");
+                setting.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                setting.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                Configuration configuration = new Configuration();
+                configuration.setProperties(setting);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return  sessionFactory;
+    }
+
+    public static Connection createConnection(){
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Cannot create connection.", e);
         }
     }
-
-    public static void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-
 }
